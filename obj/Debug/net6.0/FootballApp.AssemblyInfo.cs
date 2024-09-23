327d0309b882016eb7cf07e7ff3d73ad06eed3df
CHAYMAE JILLA <jillachaymae@gmail.com>
2024-09-23 22:14:09 +0200

    commit

--- obj/Debug/net6.0/FootballApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/FootballApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FootballApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c24c99714ae4359e567de78662006d1e5295ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FootballApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FootballApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
